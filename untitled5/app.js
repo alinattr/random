e36126cb73b5ad8b088c7f5dee3d8396297ce750
Alina Fedorova <87044568+alinattr@users.noreply.github.com>
2021-10-27 13:23:21 +0300

    Add files via upload

---
+++ untitled5/app.js
@@ -0,0 +1,21 @@
+const checkBtn = document.querySelector("button");
+let string_inner_container = document.getElementById('string_inner_container')
+
+
+
+checkBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+string_inner_container.innerHTML = 'Ticket ID: ' + createRandom(7)
+});
+
+    function createRandom(string_length) {
+        let randomString = ' ';
+        let characters = '0123456789'
+        for (let i = 0; i < string_length; i++) {
+            randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return randomString
+
+    }
+
